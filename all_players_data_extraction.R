d36d845d3bbdfa7e1a2ffe095e89a0f49632d548
Turing551 <meeturfrndashok8@gmail.com>
2019-04-19 20:18:32 +0530

    Player data extraction

---
+++ all_players_data_extraction.R
@@ -0,0 +1,73 @@
+# This file extracts the data from web for all the players required
+# Time taken for running ~several minutes (Not advised to run this for testing)
+
+# Load the required libraries
+library(xml2)
+library(rvest)
+library(XML)
+library(RCurl)
+library(rlist)
+library(dplyr)
+
+# Required Data
+players_urls_df = read.csv("Urls_Players.csv")
+players_urls = list(as.character(players_urls_df$x))[[1]]
+all_players_df = data.frame()
+
+# Looping over all the players
+for (index_player in c(1:length(players_urls))){
+  print(paste("Player",index_player,"out of",nrow(players_urls_df), sep=" "))
+  player_url = players_urls[index_player]
+  player_page<-read_html(player_url)
+  player_page_information <-html_nodes(player_page, "p")
+  player_country = player_page_information[grep("player.*country", player_page_information, ignore.case = TRUE)][1]%>%html_text()
+  player_name = strsplit(player_page_information[grep("full.*name", player_page_information, ignore.case = TRUE)][1]%>%html_text(), "\n")[[1]][2]
+  
+  player_batting_style_data = player_page_information[grep("batting.*style", player_page_information, ignore.case = TRUE)][1]%>%html_text()
+  player_batting_style = ifelse(length(player_batting_style_data) == 0, NA, player_batting_style_data)
+  player_bowling_style_data = player_page_information[grep("bowling.*style", player_page_information, ignore.case = TRUE)][1]%>%html_text()
+  player_bowling_style = ifelse(length(player_bowling_style_data) == 0, NA, player_bowling_style_data)
+  
+  tables<-html_nodes(player_page, "table")
+  
+  # Batting Statistics
+  table_batting <- html_table(tables[1], fill = TRUE)
+  if (length(table_batting) == 0){
+    next
+  }
+  if (!("ODIs" %in% table_batting[[1]][,1])){
+    next
+  }
+  rownames(table_batting[[1]]) = table_batting[[1]][,1]
+  table_batting = table_batting[[1]][,-1]
+  if (!"ODIs" %in% rownames(table_batting)){
+    next
+  }
+  odi_batting = table_batting["ODIs",]
+  odi_batting[odi_batting == "-" | odi_batting == " "] = NA
+  odi_batting$HS = as.numeric(gsub("\\*", "", odi_batting$HS))
+  odi_batting[] <- lapply(odi_batting, function(x) as.numeric(x))
+  
+  # Bowling Statistics
+  table_bowling <- html_table(tables[2], fill = TRUE)
+  rownames(table_bowling[[1]]) = table_bowling[[1]][,1]
+  table_bowling = table_bowling[[1]][,-1]
+  odi_bowling = table_bowling["ODIs",]
+  odi_bowling$BBI = NULL
+  odi_bowling$BBM = NULL
+  odi_bowling[odi_bowling == "-" | odi_bowling == " "] = NA
+  odi_bowling[] <- lapply(odi_bowling, function(x) as.numeric(x))
+  
+  player_statistics = merge(odi_batting, odi_bowling, by=0, suffixes = c("_Bat", "_Bowl"))
+  player_statistics$Full_Name = player_name
+  player_statistics$Country = player_country
+  player_statistics$Batting_Style = player_batting_style
+  player_statistics$Bowling_Style = player_bowling_style
+  player_statistics$Row.names = NULL
+  
+  all_players_df = bind_rows(all_players_df, player_statistics)
+  
+} # end player for loop
+
+# Writing the required data
+#write.csv(all_players_df, "Data_Players.csv")
